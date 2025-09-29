f16be6e3a4fdb29aaa5687fae790b3c605a58159
nurdas <nurdas00@gmail.com>
2025-09-29 09:57:05 +0600

    add exchange into order request

--- src/main/java/nur/kg/domain/request/OrderRequest.java
+++ src/main/java/nur/kg/domain/request/OrderRequest.java
@@ -1,5 +1,6 @@
 package nur.kg.domain.request;
 
+import nur.kg.domain.enums.Exchange;
 import nur.kg.domain.enums.OrderType;
 import nur.kg.domain.enums.Side;
 import nur.kg.domain.enums.Symbol;
@@ -7,4 +8,4 @@ import nur.kg.domain.enums.Symbol;
 import java.math.BigDecimal;
 
 public record OrderRequest(String id, Symbol symbol, Side side, OrderType type,
-                           BigDecimal qty, BigDecimal limitPrice, String reason) {}
+                           BigDecimal qty, BigDecimal limitPrice, String reason, Exchange exchange) {}
