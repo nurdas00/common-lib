047f2f8944830a595c81533e5d5f1f9479f0f322
nurdas <nurdas00@gmail.com>
2026-01-18 23:22:33 +0600

    add fields into OrderRequest

--- src/main/java/nur/kg/domain/request/OrderRequest.java
+++ src/main/java/nur/kg/domain/request/OrderRequest.java
@@ -1,5 +1,6 @@
 package nur.kg.domain.request;
 
+import lombok.Builder;
 import nur.kg.domain.enums.Exchange;
 import nur.kg.domain.enums.OrderType;
 import nur.kg.domain.enums.Side;
@@ -7,5 +8,7 @@ import nur.kg.domain.enums.Symbol;
 
 import java.math.BigDecimal;
 
+@Builder
 public record OrderRequest(String id, Symbol symbol, Side side, OrderType type,
-                           BigDecimal qty, BigDecimal limitPrice, String reason, Exchange exchange) {}
+                           BigDecimal qty, BigDecimal limitPrice, String reason,
+                           Exchange exchange, String botId, BigDecimal tp, BigDecimal sl) {}
